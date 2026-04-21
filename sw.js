fa3963c2ed84aeff21ed853addef925ddcf35c11
Belmonte <60616737+GuilhermeBelmonte@users.noreply.github.com>
2026-04-21 16:22:54 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE = 'aulas-v1';
+const FILES = ['./controle_aulas.html', './manifest.json'];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(c) { return c.addAll(FILES); })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(cached) {
+      return cached || fetch(e.request);
+    })
+  );
+});
